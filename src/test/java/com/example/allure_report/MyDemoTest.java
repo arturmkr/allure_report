f65eb883c88c9210c303ea265354442e9e7b6459
amkrtychian <artur.mkr@gmail.com>
2024-07-23 23:05:29 +0300

    init

--- src/test/java/com/example/allure_report/MyDemoTest.java
+++ src/test/java/com/example/allure_report/MyDemoTest.java
@@ -1,16 +1,22 @@
 package com.example.allure_report;
 
+import groovy.util.logging.Slf4j;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Random;
 
 import static io.restassured.RestAssured.given;
 
+@Slf4j
 public class MyDemoTest {
+
+    private static final Logger log = LoggerFactory.getLogger(MyDemoTest.class);
 
     @Test
     void sendDataTest() {
@@ -18,6 +24,7 @@ public class MyDemoTest {
 
         Random rand = new Random();
         int randomNum = 1000 + rand.nextInt(1001);
+        log.info("randomNum: {}", randomNum);
 
         given()
                 .filters(new AllureRestAssured(), new RequestLoggingFilter(), new ResponseLoggingFilter())
