a59536a6a20db1a41d71e1431011c84a0e333e14
amkrtychian <artur.mkr@gmail.com>
2024-12-16 14:11:40 +0200

    init

--- src/test/java/com/example/allure_report/MyDemoTest.java
+++ src/test/java/com/example/allure_report/MyDemoTest.java
@@ -3,37 +3,39 @@ package com.example.allure_report;
 import groovy.util.logging.Slf4j;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Random;
 
 import static io.restassured.RestAssured.given;
 
 @Slf4j
 public class MyDemoTest {
 
-    private static final Logger log = LoggerFactory.getLogger(MyDemoTest.class);
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "https://petstore.swagger.io/";
+    }
 
     @Test
-    void sendDataTest() {
-        RestAssured.baseURI = "https://petstore.swagger.io/";
-
-        Random rand = new Random();
-        int randomNum = 1000 + rand.nextInt(1001);
-        log.info("randomNum: {}", randomNum);
-
+    void sendFailure() {
         given()
-                .filters(new AllureRestAssured(), new RequestLoggingFilter(), new ResponseLoggingFilter())
+                .filters(new AllureRestAssured())
                 .when()
-                .get("v2/pet/" + randomNum)
+                .get("v2/pet/481345")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    void sendSuccess() {
+        given()
+                .filters(new AllureRestAssured())
+                .when()
+                .queryParam("status", "available")
+                .get("v2/pet/findByStatus")
                 .then()
                 .assertThat()
                 .statusCode(200);
     }
 }
-
-////
